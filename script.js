4b39c2c6ff0aeed46ab03708b64d6e1217929590
Denton Flake <me@dentonflake.com>
2024-10-02 13:42:45 -0700

    added some data structures

---
+++ script.js
@@ -0,0 +1,27 @@
+// numbers
+let x = 5
+let y = 10
+
+// strings
+let girlfriend = "gracie"
+let boyfriend = "denton"
+
+// dictionaries
+let gracie = {
+    name: "Gracie",
+    age: 21,
+    hobbies: ["hiking", "photography", "mountains"],
+    fiance: boyfriend
+}
+
+// arrays
+let people = [girlfriend, boyfriend]
+
+// operations
+let a = x + y
+let s = x - y
+let d = a / s
+let m = a * s
+
+// conditions
+let 
